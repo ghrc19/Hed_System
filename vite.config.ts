bc8a7014c18734e57f73cfe9028ee4d7e212e09a
ghrc19 <69125313+ghrc19@users.noreply.github.com>
2025-05-28 23:59:21 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || "/",
+  base: process.env.VITE_BASE_PATH || "/Hed_System",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
